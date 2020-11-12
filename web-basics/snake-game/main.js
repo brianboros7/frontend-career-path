809d7102c5ad804ec2d4a24518ffc8abd1f831a8
Brian Boros <Bpower@tests-MBP.attlocal.net>
2020-11-11 18:09:40 -0600

    Snake Game almost finished, added the createGrid, more to the move function, and startGame functions

--- web-basics/snake-game/main.js
+++ web-basics/snake-game/main.js
@@ -1,11 +1,19 @@
 const grid = document.querySelector('.grid')
 const startButton = document.getElementById('start')
-const score = document.getElementById('score')
+const scoreDisplay = document.getElementById('score')
 
 let squares = []
-let currentSnake = [0,1,2]
+let currentSnake = [2,1,0]
 let direction = 1 
 let width = 10
+let appleIndex = 0 
+let score = 0 
+let intervalTime = 1000
+let speed = 0.9
+let timeId = 0
+
+
+//=======================================================
 
 function createGrid() {
 
@@ -20,33 +28,95 @@ function createGrid() {
 }
 createGrid()
 
+
+
+//===========================================================
+
+// Create the Start and Restart logic 
+function startGame() {
+    timeId = setInterval(move, intervalTime)
+}
+
+
+
+//==================================-------------------------
+
 currentSnake.forEach(index => squares[index].classList.add('snake'))
-// let's make the snake move 
+// Make the snake move 
 function move() {
     if (
-        (currentSnake[0] + width >= width*width && direction === width)
-        (currentSnake[0]  % width === width-1 && direction === 1)
-        (currentSnake[0]  % width === 0 && direction === -1)
-        (currentSnake[0] - width < 0 && direction === -width)
-        squares[currentSnake[0] + direction].clssList.contains('snake')
+        (currentSnake[0] + width >= width*width && direction === width) ||
+        (currentSnake[0]  % width === width-1 && direction === 1) ||
+        (currentSnake[0]  % width === 0 && direction === -1) ||
+        (currentSnake[0] - width < 0 && direction === -width) ||
+        squares[currentSnake[0] + direction].classList.contains('snake')
     )
     return clearInterval(timeId)
 
 
+
+
+    // REmove the last element from the currentSnake array
     const tail = currentSnake.pop()
     console.log(tail)
     console.log(currentSnake)
-
+    
+    // Remove the styling from the last element 
     squares[tail].classList.remove('tail')
+    
 
+    // Add a square in the direction we want the snake to go
     currentSnake.unshift(currentSnake[0] + direction)
-    console.log(currentSnake)
+    
+
+    // Make the snakes head get the apple 
+    if (squares[currentSnake[0]].classList.contains('apple')) { 
+        // remove the class of apple
+        squarespcurrentSnake[0].classList.remove('apple')
+
+        // Grow the snake by add the class snake to it
+        squares[tail].classList.add('snake')
+        console.log(tail)
+
+        //Grow the snake array 
+        currentSnake.push(tail)
+        console.log(currentSnake)
+        
+        //Generate new Apple 
+        generateApple()
+
+        //Add one to the score
+        score++
+
+        //Display the score 
+        scoreDisplay.textContent = score
+
+        //Speed up the snake 
+        clearInterval(timeId)
+        console.log(intervaleTime)
+        intervalTime = intervalTime * 0.9 
+        timeId = setInterval(move, intervalTime)
+    } 
 
     squares[currentSnake[0]].classList.add('snake')
 }
 move()
 
-let timeId = setInterval(move, 1000)
+
+
+// ==============================================================
+
+
+// Generate a random apple within the game 
+function generateApple() {
+    do {
+        appleIndex = Math.floor(Math.random() * squares.length) 
+    } while (squares[appleIndex].classList.contains*('snake'))
+    squares[appleIndex].classList.add('apple')
+}
+
+
+// =======================================
 
 // Keycodes
 // Right arrow: 39
