9f5e0b37c2dd27b199f387fb88d55d25630e5270
Brian Boros <Bpower@tests-MBP.attlocal.net>
2020-12-08 16:02:13 -0600

    fixed two fucntions for the web basics and advnaced web btns and both are function now.

--- main.js
+++ main.js
@@ -3,10 +3,24 @@ const advancedWebBtn = document.getElementById('advanecdWebBtn')
 
 
 const menu = document.getElementById('menu')
-function toggleDropdown() {
-    menu.hidden = !menu.hidden
-    menu.addEventListener("click", function(e) {
-        e.menu.hidden
+function webBasicsToggle(webBasicsBtn) {
+    wbMenu.hidden = !wbMenu.hidden
+    weBasicsBtn = document.addEventListener('click', function() {
+        console.log('web basics clicked ')
     })
-    console.log('clicked')
+    wbMenu.addEventListener("click", function(e) {
+        e.wbMenu.hidden
+    })
+    console.log('menu displayed')
+} 
+
+function advancedWebToggle(advancedWebBtn) {
+    adMenu.hidden = !adMenu.hidden
+    advancedWebBtn = document.addEventListener('click', function() {
+        console.log('advanced web clicked')
+    })
+    adMenu.addEventListener("click", function(e) {
+        e.adMenu.hidden
+    })
+    console.log('menu displayed')
 } 
