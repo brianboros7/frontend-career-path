ef6c6b4f96b644ae62a6e2e7002a3a9ebb7dd405
Brian Boros <Bpower@tests-MacBook-Pro.local>
2020-12-10 15:18:32 -0600

    made project nav bttutons dynamic with vanilla JS, and styled li better

--- main.js
+++ main.js
@@ -1,26 +1,11 @@
-const webBasicsBtn = document.getElementById('webBasicsBtn')
-const advancedWebBtn = document.getElementById('advanecdWebBtn')
 
+function showMenu(btn) {
+    const menus = document.querySelectorAll('.menu')
+    menus.forEach(function(menu) {
+        menu.hidden = true 
+    })
+    let open = btn.id
 
-const menu = document.getElementById('menu')
-function webBasicsToggle(webBasicsBtn) {
-    wbMenu.hidden = !wbMenu.hidden
-    weBasicsBtn = document.addEventListener('click', function() {
-        console.log('web basics clicked ')
-    })
-    wbMenu.addEventListener("click", function(e) {
-        e.wbMenu.hidden
-    })
-    console.log('menu displayed')
-} 
-
-function advancedWebToggle(advancedWebBtn) {
-    adMenu.hidden = !adMenu.hidden
-    advancedWebBtn = document.addEventListener('click', function() {
-        console.log('advanced web clicked')
-    })
-    adMenu.addEventListener("click", function(e) {
-        e.adMenu.hidden
-    })
-    console.log('menu displayed')
-} 
+    let openMenu = document.querySelector('.' + open) 
+    openMenu.hidden = false 
+}
