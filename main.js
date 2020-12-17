c320ff941bae9958ee76173ccf4bab2f3d54fc14
Brian Boros <Bpower@tests-MBP.attlocal.net>
2020-12-17 15:06:18 -0600

    got Usplash url's correct and mobile and desktop ready

--- main.js
+++ main.js
@@ -16,22 +16,24 @@ function showMenu(btn) {
 
 
 /* Unsplash Fetch API */
+// Single image url ex. - https://api.unsplash.com/photos/WkfDrhxDMC8?client_id=gK52De2Tm_dL5o1IXKa9FROBAJ-LIYqR41xBdlg3X2k
+// full URL -  https://api.unsplash.com/photos/random/?query=developer&client_id=gK52De2Tm_dL5o1IXKa9FROBAJ-LIYqR41xBdlg3X2k
 window.addEventListener('load', loadImg);
-
 function loadImg() {
-    const url = "https://api.unsplash.com/search/photos?query=coffee&per_page=20&client_id=gK52De2Tm_dL5o1IXKa9FROBAJ-LIYqR41xBdlg3X2k";
+    const url = "https://api.unsplash.com/photos/WkfDrhxDMC8?client_id=gK52De2Tm_dL5o1IXKa9FROBAJ-LIYqR41xBdlg3X2k";
     const imageDiv = document.querySelector('.image');
+    let listOfIDs = []
     fetch(url).then(response => {
         return response.json();
     })
     .then(data => {
-        for (let i = 0; i < data.results.length; i++) { 
-            // Fetch only image that you want by using id. Example : https://unsplash.com/photos/6VhPY27jdps, id = '6VhPY27jdps'   
-            if (data.results[i].id == '6VhPY27jdps') {
-                let imageElement = document.createElement('img')
-                imageElement.src = data.results[i].urls.thumb
-                imageDiv.append(imageElement)
-            }
-        }
+            // for (let i = 0; i < data.results.length; i++) { 
+        // Fetch only image that you want by using id. Example : https://unsplash.com/photos/6VhPY27jdps, id = '6VhPY27jdps'   
+            // if (data.results[i].id == 'WkfDrhxDMC8') {
+        let imageElement = document.createElement('img')
+        imageElement.src = data.urls.thumb
+        imageDiv.append(imageElement)
+        
+        
     })
 }
